9ac07e03a09dd43b32b7c895e13c48fa459d5c49
Helder <helder@lamoulade.com>
2016-08-16 19:16:55 +0100

    removed unwanted key

--- app/data/questions.js
+++ app/data/questions.js
@@ -4,48 +4,45 @@ var data = [{
 	"correct": 1,
 	"answers": [{
 		"id": 1,
-		"answer": "Lorem ipsum a" 
+		"answer": "Lorem ipsum a"
 	}, {
 		"id": 2,
-		"answer": "7Lorem ipsum b" 
+		"answer": "7Lorem ipsum b"
 	}, {
 		"id": 3,
-		"answer": "Lorem ipsum c" 
-	}],
-	"selected_answer" : ""
+		"answer": "Lorem ipsum c"
+	}]
 }, {
 	"id": 2,
 	"question": "2Yuga zubi question ?",
 	"correct": 3,
 	"answers": [{
 		"id": 1,
-		"answer": "zLorem ipsum a" 
+		"answer": "zLorem ipsum a"
 	}, {
 		"id": 2,
-		"answer": "xzLorem ipsum b" 
+		"answer": "xzLorem ipsum b"
 	}, {
 		"id": 3,
-		"answer": "zxLorem ipsum c" 
+		"answer": "zxLorem ipsum c"
 	}, {
 		"id": 4,
-		"answer": "zxLorem ipsum d" 
-	}],
-	"selected_answer" : ""
+		"answer": "zxLorem ipsum d"
+	}]
 }, {
 	"id": 3,
 	"question": "3Lopa zapa question ?",
 	"correct": 0,
 	"answers": [{
 		"id": 1,
-		"answer": "weLorem ipsum al" 
+		"answer": "weLorem ipsum al"
 	}, {
 		"id": 2,
-		"answer": "xLorem ipsum bel" 
+		"answer": "xLorem ipsum bel"
 	}, {
 		"id": 3,
-		"answer": "xLorem ipsum cel" 
-	}],
-	"selected_answer" : ""
+		"answer": "xLorem ipsum cel"
+	}]
 }];
 
 export default data;
