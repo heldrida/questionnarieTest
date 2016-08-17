1552334023ad9cf10f049f4c1bdfeecadd332140
Helder <heldrida@gmail.com>
2016-08-17 02:17:32 +0100

    added more questions and answers; created real questions intead of lorem ipsum

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,47 +1,84 @@
 var data = [{
 	"id": 1,
-	"question": "1Supa dupa question ?",
+	"question": "Who wrote the book Catch-22 ?",
 	"correct": 2,
 	"answers": [{
 		"id": 1,
-		"text": "Lorem ipsum a"
+		"text": "Lois Peterloski"
 	}, {
 		"id": 2,
-		"text": "7Lorem ipsum b"
+		"text": "Joseph Heller"
 	}, {
 		"id": 3,
-		"text": "Lorem ipsum c"
+		"text": "Anna C. Kennedy"
 	}]
 }, {
 	"id": 2,
-	"question": "2Yuga zubi question ?",
+	"question": "What is English dairy farmer Michael Eavis famous for ?",
 	"correct": 3,
 	"answers": [{
 		"id": 1,
-		"text": "zLorem ipsum a"
+		"text": "Nesquik"
 	}, {
 		"id": 2,
-		"text": "xzLorem ipsum b"
+		"text": "Cadboury Dairy Milk"
 	}, {
 		"id": 3,
-		"text": "zxLorem ipsum c"
+		"text": "Glastonbury Festival"
 	}, {
 		"id": 4,
-		"text": "zxLorem ipsum d"
+		"text": "Beer garden"
 	}]
 }, {
 	"id": 3,
-	"question": "3Lopa zapa question ?",
+	"question": "Which group had a 1980s hit with the song Ghost Town ?",
 	"correct": 1,
 	"answers": [{
 		"id": 1,
-		"text": "weLorem ipsum al"
+		"text": "The Specials"
 	}, {
 		"id": 2,
-		"text": "xLorem ipsum bel"
+		"text": "Michael Jackson"
 	}, {
 		"id": 3,
-		"text": "xLorem ipsum cel"
+		"text": "Bad Brains"
+	}, {
+		"id": 4,
+		"text": "The Ramones"
+	}]
+}, {
+	"id": 4,
+	"question": "Jimmy Page is an English musician who formed which rock band in 1968 ?",
+	"correct": 3,
+	"answers": [{
+		"id": 1,
+		"text": "Madonna"
+	}, {
+		"id": 2,
+		"text": "Jon Bon Jovi"
+	}, {
+		"id": 3,
+		"text": "Led Zeppelin"
+	}, {
+		"id": 4,
+		"text": "Scorpions"
+	}, {
+		"id": 5,
+		"text": "Bob Marley & The Wailers"
+	}]
+}, {
+	"id": 5,
+	"question": "Which Radiohead album has been cited by musicians as one of the greatest albums of all time ?",
+	"correct": 2,
+	"answers": [{
+		"id": 1,
+		"text": "Musical Youth"
+	}, {
+		"id": 2,
+		"text": "OK Computer"
+	}, {
+		"id": 3,
+		"text": "The universe"
 	}]
 }];
 
